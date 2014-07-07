e69a37e5382dbe7c622f0fd01216ac746e73587d
3axap123 <3axap123@mail.ru>
2014-07-07 14:32:29 +0300

    rats running

--- DZ12/RATS_RUNNING/main.c
+++ DZ12/RATS_RUNNING/main.c
@@ -3,8 +3,8 @@
 
 int main()
 {
-    int VanyaSpeed,VladSpeed;
-    double VladUstal,IvanUstal,S;
+    int VanyaSpeed,VladSpeed,S;
+    double VladUstal,IvanUstal,VladUstalspeed,IvanUstalspeed,tIvan,tVlad;
     printf("VVEDITE SKOROST' IVANA\n");
     scanf("%d",&VanyaSpeed);
     printf("VVEDITE SKOROST' VLADA\n");
@@ -12,30 +12,29 @@ int main()
     printf("VVEDITE RASSTOYANIE\n");
     scanf("%d",&S);
 
-    VladUstal = S/VanyaSpeed;
-    IvanUstal = S/VladSpeed;
-    printf("%lg",VladUstal);
-    printf("%lg",IvanUstal);
+    IvanUstal=S*0.63;
+    VladUstal=S*0.57;
 
+    printf("IVAN USTANET CHEREZ %g METROV\n",IvanUstal);
+    printf("VLAD USTANET CHEREZ %g METROV\n\n\n", VladUstal);
 
+    IvanUstalspeed=VanyaSpeed*0.3;
+    VladUstalspeed=VladSpeed*0.28;
 
+    printf("IVAN USTAL I BEJIT %g M/C\n",IvanUstalspeed);
+    printf("VLAD USTAL I BEJIT %g M/C\n\n\n",VladUstalspeed);
 
+    tIvan=(S-IvanUstal)/IvanUstalspeed;
+    tVlad=(S-VladUstal)/VladUstalspeed;
+    printf("IVAN PRIBEJIT CHEREZ %g SEKUND\n",tIvan);
+    printf("VLAD PRIBEJIT CHEREZ %g SEKUND\n\n\n",tVlad);
 
+    if (tIvan>tVlad)
 
+        printf("VLAD - CHAMPION!!!");
 
+    else
+        printf("IVAN - CHAMPION!!!");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    return 0;
+        return 0;
 }
